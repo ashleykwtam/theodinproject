50cb1d1df8ef952f06e5131032b6f3918acbac8f
Ashley <ashleykwtam@gmail.com>
2016-09-08 14:55:38 -0400

    working on newgrid function

--- WD101/JS-EtchASketch/script.js
+++ WD101/JS-EtchASketch/script.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
-	for (var x = 0; x < 16; x++) {
-		for (var y = 0; y < 16; y++) {
-			$("<div>").addClass("grid").appendTo("#container");
-		}
-	}
+	initialGrid();
 
 	$(".grid").hover(function(){
 		$(this).addClass("highlight")
@@ -12,4 +8,31 @@ $(document).ready(function(){
 	$(".cleargrid").click(function(){
 		$(".grid").removeClass("highlight")
 	});
+
+	$(".newgrid").click(function(){
+		newGrid();
+	});
 });
+
+function initialGrid() {
+		for (var x = 0; x < 16; x++) {
+		for (var y = 0; y < 16; y++) {
+			$("<div>").addClass("grid").appendTo("#container");
+		}
+	}
+}
+
+function newGrid() {
+	
+	var newgrid = prompt("Please enter new size", "16");
+
+	var newheight = (600 / newgrid) - 3
+	var newwidth = (600 / newgrid) - 3
+
+	if (newgrid < 65) {
+		$(".grid").css("height", newheight);
+		$(".grid").css("width", newwidth);
+	} else {
+		alert("I'm sorry, that number is too big. Enter something smaller.")
+	}
+} 
