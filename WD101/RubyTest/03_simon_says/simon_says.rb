14c223bdc0477d37b4c0966b8c3151faf57b345d
Ashley <ashleykwtam@gmail.com>
2016-09-21 17:59:17 -0400

    completed simon says and pig latin exercise

--- WD101/RubyTest/03_simon_says/simon_says.rb
+++ WD101/RubyTest/03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
 #write your code here
+def echo(word)
+	word
+end
+
+def shout(word)
+	word.upcase
+end
+
+def repeat(word, times =2)
+	([word] * times).join(" ")
+end
+
+def start_of_word(start, stop)
+	start[0...stop]
+end
+
+def first_word(word)
+	word.split(" ").first
+end
+
+def titleize(s)
+	words = s.split.each do |word|
+		if word != "and" and word != "over" and word != "the"
+			word.capitalize!
+		else
+			word.downcase!
+		end
+	end
+	words.first.capitalize!
+	words.join(" ")
+end
--- WD101/RubyTest/04_pig_latin/pig_latin.rb
+++ WD101/RubyTest/04_pig_latin/pig_latin.rb
@@ -1,1 +1,29 @@
 #write your code here
+def translate(word)
+	words = word.split(" ")
+	words.map! do |word|
+		translate_word(word)
+	end
+	words.join(" ")
+end
+
+def vowel?(letter)
+	"aeiouy".include? letter
+end
+
+def translate_word(word)
+	x = word.length
+	if vowel?(word[0])
+		word + "ay"
+	elsif word[0..1] == "qu"
+		word[2..(x-1)] + word[0..1] + "ay"
+	elsif word[1..2] == "qu"
+		word[3..(x-1)] + word[0..2] + "ay"
+	elsif !vowel?(word[0]) && !vowel?(word[1]) && !vowel?(word[2]) # word begins with 3 consonants
+		word[3..(x-1)] + word[0..2] + "ay"
+	elsif !vowel?(word[0]) && !vowel?(word[1]) # word begins with 2 consonants
+		word[2..(x-1)] + word[0..1] + "ay"
+	else !vowel?(word[0]) # word begins with consonant
+		word[1..(x-1)] + word[0] + "ay"
+	end
+end
