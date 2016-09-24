589a60b1fb07ed5aed05c582dd16de228749c474
Ashley <ashleykwtam@gmail.com>
2016-09-24 13:01:11 -0400

    completed book titles and timer

--- WD101/RubyTest/05_book_titles/book.rb
+++ WD101/RubyTest/05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
 # write your code here
+	attr_reader :title
+
+	def title=(new_title)
+		words = new_title.capitalize!.split(" ")
+		words.map! do |word|
+			if ["the", "a", "an", "and", "of", "in"].include? word
+				word
+			else
+				word.capitalize
+			end
+		end
+		@title = words.join(" ")
+	end
 end
--- WD101/RubyTest/06_timer/timer.rb
+++ WD101/RubyTest/06_timer/timer.rb
@@ -1,3 +1,30 @@
 class Timer
   #write your code here
+  attr_writer :seconds, :minutes, :hours
+
+  def initialize
+  	@seconds = 0
+  	@minutes = 0
+  	@hours = 0
+  end
+
+  def time_string
+  	pad(hours) + ":" + pad(minutes) + ":" + pad(seconds)
+  end
+
+  def seconds
+  	@seconds % 60
+  end
+
+  def minutes
+  	@seconds / 60 % 60
+  end
+
+  def hours
+  	@seconds / 60 / 60 % 60
+  end
+
+  def pad(number)
+  	"%02d" % number
+  end
 end
