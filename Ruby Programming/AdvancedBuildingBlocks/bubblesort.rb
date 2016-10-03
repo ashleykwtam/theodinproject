e385a4f8a74158e91ae3d4c70f19abf61818c157
Ashley <ashleykwtam@gmail.com>
2016-10-03 18:44:07 -0400

    bubble sort accepts block

--- Ruby Programming/AdvancedBuildingBlocks/bubblesort.rb
+++ Ruby Programming/AdvancedBuildingBlocks/bubblesort.rb
@@ -16,3 +16,30 @@ def bubblesort(array)
 end
 
 bubblesort([20, 4, 2, 19, 100, 3, 50])
+
+
+
+# Now create a similar method called #bubble_sort_by which sorts an array but accepts a block. 
+# The block should take two arguments which represent the two elements currently being compared. 
+# Expect that the block's return will be similar to the spaceship operator you learned about before 
+# -- if the result of the block is negative, the element on the left is "smaller" than the 
+# element on the right. 0 means they are equal. A positive result means the left element is greater. 
+# Use this to sort your array.
+
+def bubble_sort_by(array)
+	swap = true
+	while swap
+		swap = false
+		for i in 1...array.length do
+			if yield(array[i], array[i-1]) < 0
+				array[i], array[i-1] = array[i-1], array[i]
+				swap = true
+			end
+		end
+	end
+	p array
+end
+
+bubble_sort_by(["howdy", "hello", "hi", "bye", "goodbye"]) do |left, right|
+	left.length - right.length
+end
